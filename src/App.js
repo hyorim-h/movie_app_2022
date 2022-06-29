5d6ce9b3c24acf86b3218acde1dccd824ad32468
hyorim-Han <ekqls0458@naver.com>
2022-06-29 21:40:44 +0900

    03-3 두 번째 리액트 기초 개념: JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+function Potato() {
+  return <h1>I like Potato</h1>
+}
+
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
+      <Potato />
     </div>
   );
 }
