48d6e40c7d991af99f9b9a58161bbd36b13ddc70
hyorim-Han <ekqls0458@naver.com>
2022-06-29 22:41:11 +0900

    04-3 음식 앱 이리저리 만지고, 고쳐보기

--- src/App.js
+++ src/App.js
@@ -4,29 +4,44 @@ function Food({ name, picture }) {
   return ( 
     <div>
       <h2>I like {name}</h2>
-      <img src={picture} />
+      <img src={picture} alt={name}/>
     </div>
   );
 }
 
 const foodILike = [
-  {name: 'kimchi',
-   image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',},
-   {name: 'pizza',
-   image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',},
-   {name: 'doncasu',
-   image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',},
-   {name: 'kimbap',
-   image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',},
-   {name: 'bibimbap',
-   image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',},
+  {
+    id: 1,
+    name: 'kimchi',
+    image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',
+  },
+  {
+    id: 2,
+    name: 'pizza',
+    image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',
+  },
+  {
+    id: 3,
+    name: 'doncasu',
+    image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',
+  },
+  {
+    id: 4,
+    name: 'kimbap',
+    image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',
+  },
+  {
+    id: 5,
+    name: 'bibimbap',
+    image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',
+  },
 ];
 
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image}/>))}
+        <Food key={dish.id} name={dish.name} picture={dish.image}/>))}
     </div>
   );
 }
