3b20664b49f75ff62c1f96d984e0d51878bb8602
hyorim-Han <ekqls0458@naver.com>
2022-06-30 15:16:28 +0900

    05-1 state로 숫자 증감 기능 만들어 보기

--- src/App.js
+++ src/App.js
@@ -1,63 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({ name, picture, rating }) {
-  return ( 
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count : 0,
+  };
+
+  add = () =>{
+    console.log('add');
+  };
+
+  minus = () => {
+    console.log('minus');
+  };
+
+  render(){
+    return (
+      <div>
+        <h1>The Number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'kimchi',
-    image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'pizza',
-    image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: 'doncasu',
-    image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: 'kimbap',
-    image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',
-    rating: 4.7,
-  },
-  {
-    id: 5,
-    name: 'bibimbap',
-    image: 'https://img.hankyung.com/photo/202108/99.27186019.1.jpg',
-    rating: 4.6,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image}
-        rating={dish.rating} />))}
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
 
 export default App;
